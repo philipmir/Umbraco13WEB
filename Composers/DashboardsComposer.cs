353c9d270bc7456092b7732b26e7a48ff0361c61
philipmir <philipmir@hotmail.com>
2025-01-30 11:42:26 +0100

    7 commit

---
+++ Composers/DashboardsComposer.cs
@@ -0,0 +1,17 @@
+using Our.Umbraco.TheDashboard.Counters.Implement;
+using Our.Umbraco.TheDashboard.Extensions;
+
+using Umbraco.Cms.Core.Composing;
+using Umbraco.Cms.Core.Dashboards;
+
+namespace UM13WEBSITE.Composers;
+
+public class DashboardsComposer : IComposer
+{
+    public void Compose(IUmbracoBuilder builder)
+    {
+        builder.Dashboards().Remove<ContentDashboard>();
+        builder.TheDashboardCounters().Remove<MembersTotalDashboardCounter>()
+            .Remove<MembersNewLastWeekDashboardCounter>();
+    }
+}
